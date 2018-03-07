e69b8ad95fdc9ae0a0259e1f7171d311deebfe4e
Donald Faul <faudo01@ca.com>
2018-03-06 16:27:34 -0800

    faudo01 6 March 2018
    changes

--- index.js
+++ index.js
@@ -148,8 +148,6 @@ var Mailgun = require('mailgun').Mailgun;
 var cons = require('console');
 var Sequelize = require('sequelize');
 require('sequelize-isunique-validator')(Sequelize);
-var AWS = require('aws-sdk'); 
-var kms = new AWS.KMS(); 
 
 const config = {
     db_username: "gm_tools_user",
@@ -159,52 +157,22 @@ const config = {
 	db_dialect: "mssql"
 	};
 	
-//const psswd = process.env.IMAG_DB_PASSWORD;
-var buf = Buffer.from(process.env.IMAG_DB_PASSWORD, 'base64'); 
-var db = null;
+const psswd = process.env.IMAG_DB_PASSWORD;
 
-function connectDB(type, body, subject) {
-  return new Promise((resolve, reject) => {
-		if (db == null) {
-			var params = {
-				CiphertextBlob: buf
-			};
-			kms.decrypt(params, function(err, data) {
-				if (err) reject('Password Decryption Failed'); // an error occurred
-				else {
-					var psswd = data['Plaintext'];
-					console.log('Password:'+psswd);
-					db = new Sequelize(config.db_name, config.db_username, psswd,
-											  {host: config.db_host, dialect: config.db_dialect,
-											   pool: {max: 5, min: 0, acquire: 30000, idle: 10000 },
-														  operatorsAliases: false });
-					resolve(subject);
-				}; // successful response		
-			});
-		}  
-		else {
-			resolve(subject);
-		}
-  }) 
-};
+const db = new Sequelize(config.db_name, config.db_username, psswd,
+                                  {host: config.db_host, dialect: config.db_dialect,
+								   pool: {max: 5, min: 0, acquire: 30000, idle: 10000 },
+								  operatorsAliases: false });
 
 function updateDB(type, body, subject) {
   return new Promise((resolve, reject) => {
-	  
 	var jsonobj = JSON.parse(body);
 	
 	var date = new Date().toISOString();
 	var expr = '';
 	var location = '';
 	var preamble ="INSERT INTO GITHUB_EVENTS ( Application, ApplicationLocation, ResourceOwner1, ResourceOwner2, ResourceName, EventTime, EventAttributes, User_ID ) VALUES (";
-
-    // connect DB
-	var buf = Buffer.from(process.env.IMAG_DB_PASSWORD, 'base64'); 
-	var db = null;
-	var params = {
-		CiphertextBlob: buf
-	};
-	
+		
 	switch (type) {
 	case 'public':
 		location = jsonobj.repository.owner.html_url;
@@ -260,26 +228,12 @@ function updateDB(type, body, subject) {
 	if (expr && expr.length > 0) {
 		console.log('Type: '+type+'  Sender: '+jsonobj.sender.login+'\n'+body);
 		//console.log('Query: '+expr);
-		kms.decrypt(params, function(err, data) {
-			if (err) reject('Password Decryption Failed'); // an error occurred
-			else {
-				var psswd = data['Plaintext'];
-				console.log('Password:'+psswd);
-				db = new Sequelize(config.db_name, config.db_username, psswd,
-										  {host: config.db_host, dialect: config.db_dialect,
-										   pool: {max: 5, min: 0, acquire: 30000, idle: 10000 },
-													  operatorsAliases: false });
-			}; // successful response		
-		});
-		
 		db.query(expr).spread(function(results, metadata)
 		{
 			if (metadata > 0) {
-				db.close();
 				resolve(subject);
 			}
 			else {
-				db.close();
 				return reject('Insert Failed');
 			}
 		});
